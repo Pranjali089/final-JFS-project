3fb4eb62df99d06adb6e135e40e3216cc5c08c01
Pranjali089 <pranjalipatil089@gmail.com>
2023-12-12 14:03:59 +0530

    final

---
+++ laptopStore/src/app/cart.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ProductI } from './laptop-i';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  items:ProductI[]=[];
+
+  addTocart(product:ProductI){
+
+    this.items.push(product);
+  }
+
+getItems(){
+  return this.items;
+}
+
+
+
+  constructor() { }
+}
